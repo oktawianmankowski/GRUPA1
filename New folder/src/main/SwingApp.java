e6026bb6faaa304ac800aea88da8cef5310599cd
Dawid Liwocha <202214@edu.p.lodz.pl>
2017-12-17 16:13:45 +0100

    First select list

--- New folder/src/main/SwingApp.java
+++ New folder/src/main/SwingApp.java
@@ -1,6 +1,13 @@
 package main;
 
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
 
 public class SwingApp extends JFrame {
 	public SwingApp() {
@@ -12,6 +19,18 @@ public class SwingApp extends JFrame {
 	public static void main(String[] args) {
 		SwingApp simple = new SwingApp();
 
+		
+		
+		Integer[] data = {1, 2, 3, 4, 5};
+		
+		JList<Integer> list = new JList<Integer> (data); //data has type Object[]
+		list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		list.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+		list.setVisibleRowCount(-1);
+		
+		JScrollPane listScroller = new JScrollPane(list);
+		simple.add(listScroller);
+		
 		simple.setVisible(true);
 	}
 }

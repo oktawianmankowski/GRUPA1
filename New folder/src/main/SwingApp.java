ee6a2f51d4e5a69d698146c127a162c97293e282
Piotrek <piotrek120017@gmail.com>
2017-12-17 16:49:24 +0100

    second select list

--- New folder/src/main/SwingApp.java
+++ New folder/src/main/SwingApp.java
@@ -3,6 +3,7 @@ package main;
 import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JList;
@@ -26,10 +27,28 @@ public class SwingApp extends JFrame {
 		JList<Integer> list = new JList<Integer> (data); //data has type Object[]
 		list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
 		list.setLayoutOrientation(JList.HORIZONTAL_WRAP);
-		list.setVisibleRowCount(-1);
-		
+		list.setVisibleRowCount(3);
+
 		JScrollPane listScroller = new JScrollPane(list);
 		simple.add(listScroller);
+
+		Integer[] randomData = new Integer[20];
+
+		Random generator = new Random();
+		int low = 0;
+		int high = 100;
+
+		for (int i = 0; i < 20; i++){
+			randomData[i] = generator.nextInt(high - low);
+		}
+
+		JList<Integer> list2 = new JList<Integer> (randomData); //data has type Object[]
+		list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		list.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+		list.setVisibleRowCount(3);
+
+		JScrollPane listScroller2 = new JScrollPane(list2);
+		simple.add(listScroller2);
 		
 		simple.setVisible(true);
 	}
